2918603c1b554d9fde32f2fae94773c762516afe
goliatone <burgosemi@gmail.com>
2015-11-22 22:53:31 -0500

    clean up

--- health/docker.go
+++ health/docker.go
@@ -1,17 +1,24 @@
 package health
 
 import (
+	"errors"
 	"fmt"
 	"os/exec"
 	"strconv"
 	"strings"
 )
 
+const (
+	inspectRunning = "inspect --format={{.State.Running}}"
+	inspectStarted = "inspect --format={{.State.StartedAt}}"
+	inspectNetwork = "inspect --format={{.NetworkSettings.IPAddress}}"
+)
+
 //Payload struct
 type Payload struct {
-	arguments   string
-	containerID string
-	action      string
+	Arguments string
+	Target    string
+	Action    string
 }
 
 //Response struct
@@ -26,66 +33,77 @@ type Response struct {
 //for actual type check and then the rest would arguments
 //in array form. So we could call this Exec from argv or
 //whatever.
-func (p *Payload) Parse() {
-	arguments := p.arguments
+func (p *Payload) Parse() error {
+	arguments := p.Arguments
 	chunks := strings.Split(arguments, ":")
-	p.action = chunks[0]
-	p.containerID = chunks[1]
+
+	if len(chunks) != 2 {
+		return errors.New("Command formatted improperly. Expected command:arg1(:argN)")
+	}
+
+	p.Action = chunks[0]
+	p.Target = chunks[1]
+
+	return nil
 }
 
 //Exec docker container health check
-func Exec(arguments string) Response {
+func Exec(arguments string) (Response, error) {
 
-	payload := parsePayload(arguments)
+	payload, err := parsePayload(arguments)
+
+	if err != nil {
+		return Response{}, err
+	}
 
 	//wat
-	cmd := exec.Command("docker", "inspect", "--format={{.State.Running}}", payload.containerID)
+	cmd := exec.Command("docker", inspectRunning, payload.Target)
 	out, err := cmd.CombinedOutput()
 
 	if err != nil {
-		message := fmt.Sprintf("UNKNOWN - The container \"%s\" does not exist.", payload.containerID)
-		return Response{Success: false, StatusCode: 3, Message: message}
+		message := fmt.Sprintf("UNKNOWN - The container \"%s\" does not exist.", payload.Target)
+		return Response{Success: false, StatusCode: 3, Message: message}, nil
 	}
 
 	s := byteToString(out)
 	b, err := strconv.ParseBool(s)
 
 	if b == false {
-		message := fmt.Sprintf("CRITICAL - The container \"%s\" is not running.", payload.containerID)
-		return Response{Success: false, StatusCode: 2, Message: message}
+		message := fmt.Sprintf("CRITICAL - The container \"%s\" is not running.", payload.Target)
+		return Response{Success: false, StatusCode: 2, Message: message}, nil
 	}
 
 	if b == true {
-		cmd := exec.Command("docker", "inspect", "--format={{.State.StartedAt}}", payload.containerID)
+		cmd := exec.Command("docker", inspectStarted, payload.Target)
 		out, err := cmd.CombinedOutput()
 
 		if err != nil {
 			errorMessage := fmt.Sprintf("Unknown error: %s", err)
-			return Response{Success: false, StatusCode: 4, Message: errorMessage}
+			return Response{Success: false, StatusCode: 4, Message: errorMessage}, nil
 		}
 
 		network := byteToString(out)
 
-		cmd = exec.Command("docker", "inspect", "--format={{.NetworkSettings.IPAddress}}", payload.containerID)
+		cmd = exec.Command("docker", inspectNetwork, payload.Target)
 		out, err = cmd.CombinedOutput()
 
 		if err != nil {
 			errorMessage := fmt.Sprintf("Unknown error: %s", err)
-			return Response{Success: false, StatusCode: 4, Message: errorMessage}
+			return Response{Success: false, StatusCode: 4, Message: errorMessage}, nil
 		}
 
 		started := byteToString(out)
-		message := fmt.Sprintf("OK - The container \"%s\" is running. IP: %s, StartedAt: %s", payload.containerID, started, network)
-		return Response{Success: true, StatusCode: 0, Message: message}
+		message := fmt.Sprintf("OK - The container \"%s\" is running. IP: %s, StartedAt: %s", payload.Target, started, network)
+		return Response{Success: true, StatusCode: 0, Message: message}, nil
 	}
-	return Response{Success: false, StatusCode: 5, Message: "Unknown error"}
+	return Response{}, errors.New("Health check unhandled.")
 }
 
-func parsePayload(arguments string) Payload {
+func parsePayload(arguments string) (Payload, error) {
 	arguments = strings.Trim(arguments, "\n")
-	payload := Payload{arguments: arguments}
-	payload.Parse()
-	return payload
+	payload := Payload{Arguments: arguments}
+	err := payload.Parse()
+	return payload, err
 }
 
 func byteToString(b []byte) string {
